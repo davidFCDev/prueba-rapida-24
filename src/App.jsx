27ff47020378ec7b751f1fd5d5fa4dc5235d9599
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-14 13:54:03 +0200

    llamada api imagen

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { useCatFact } from "./hooks/useCatFact";
 
+const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
+
+function useCatImage({ fact }) {
+  const [imageUrl, setImageUrl] = useState();
+
+  useEffect(() => {
+    if (!fact) return;
+
+    const threeFirstWords = fact.split(" ", 3).join(" ");
+
+    fetch(
+      `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`
+    )
+      .then((res) => res.json())
+      .then((response) => {
+        const { url } = response;
+        setImageUrl(url);
+      });
+  }, [fact]);
+
+  return { imageUrl: `${CAT_PREFIX_IMAGE_URL}${imageUrl}` };
+}
+
 function App() {
   const { fact, refreshFact } = useCatFact();
+  const { imageUrl } = useCatImage({ fact });
 
   const handleClick = () => {
     refreshFact();
@@ -15,6 +40,7 @@ function App() {
       <button onClick={handleClick}>New fact</button>
 
       {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt="Random cat" />}
     </>
   );
 }
