bf7883783b4fd0f1727d71c51c2e508e00282109
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-14 13:42:29 +0200

    llamada api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
+
+const URL_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
 
 function App() {
-  return <></>;
+  const [randomFact, setRandomFact] = useState("");
+
+  useEffect(() => {
+    fetch(URL_ENDPOINT_RANDOM_FACT)
+      .then((response) => response.json())
+      .then((data) => setRandomFact(data.fact));
+  }, []);
+  
+  return (
+    <>
+      <h1>App de gatitos</h1>
+
+      <button>New fact</button>
+
+      {randomFact && <p>{randomFact}</p>}
+    </>
+  );
 }
 
 export default App;
